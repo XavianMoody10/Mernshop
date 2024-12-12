56fc19ae1049c424a1e18f7cd61e90b6fdccd0e6
Xavian Moody <XavianMoody1025@gmail.com>
2024-12-12 13:44:32 -0500

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Shop } from "./pages/Shop/Shop";
+import { Login } from "./pages/Login/Login";
+import { Signup } from "./pages/Signup/Signup";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  // All Routes
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Shop></Shop>} />
+        <Route path="/auth">
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default App;
